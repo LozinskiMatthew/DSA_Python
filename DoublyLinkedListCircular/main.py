002636eb7ebaa579f786bc9327a62e17d46c4a45
Matthew Lozinski <lozinskimatthew0@gmail.com>
2024-08-27 23:19:35 +0200

    Almost entirely done

--- DoublyLinkedListCircular/main.py
+++ DoublyLinkedListCircular/main.py
@@ -1,4 +1,7 @@
 #20 and 17 to be done, with new list (17)
+from pycparser.ply.ctokens import t_PLUSEQUAL
+from sympy.codegen.fnodes import elemental
+import numpy as np
 
 class Node:
     def __init__(self, value):
@@ -47,6 +50,7 @@ class DCLinkedList:
             self.head.prev = new_node
             self.tail.next = new_node
             self.head = new_node
+        self.length += 1
 
     def backend_element(self, value):
         new_node = Node(value)
@@ -55,6 +59,7 @@ class DCLinkedList:
         new_node.next = self.head
         new_node.prev = self.tail
         self.tail = new_node
+        self.length += 1
 
     def add_element(self, index, value):#Wherever
         if index == 0:
@@ -74,6 +79,7 @@ class DCLinkedList:
 
         if new_node.next == self.head:
             self.tail = new_node
+        self.length += 1
 
     def traverse(self):
             current = self.head
@@ -91,18 +97,19 @@ class DCLinkedList:
         current = self.head
         for index in range(self.length):
             if current.node == value:
-                return tuple(index, current)
+                return index, current
+            current = current.next
 
     def get(self, index):
-        median = self.length // 2
+        median = self.length
         if index <= median:
             current = self.head
-            for _ in range(0, index + 1):
+            for _ in range(0, index):
                 current = current.next
             return current
         elif index > median:
             current = self.tail
-            for _ in range(median + 1, self.length):
+            for _ in range(self.length - 1, index, -1):
                 current = current.prev
             return current
         else:
@@ -111,18 +118,32 @@ class DCLinkedList:
     def set(self, index, value):
         if index < 0 or index > self.length - 1:
             return -1
-        element = self.get(index)
-        element.value = value
+        elementt = self.get(index)
+        elementt.node = value
 
     def pop_first(self):
-        self.head.prev = None
-        self.head.next = None
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+            self.length -= 1
+            return
+        pop_element = self.head
         self.head = self.head.next
+        pop_element.prev = None
+        pop_element.next = None
         self.tail.next = self.head
         self.head.prev = self.tail
+        self.length -= 1
 
+    def pop_last(self):
+        to_pop = self.tail
+        self.tail = self.tail.prev
+        to_pop.next = None
+        to_pop.prev = None
+        self.head.prev = self.tail
 
-    # def pop_last(self):
+    def remove(self):
+        to_remove =
 
     # def remove_element(self):
 
@@ -144,8 +165,19 @@ if __name__ == '__main__':
     DCLinkedList.backend_element(42)
     print(DCLinkedList)
     DCLinkedList.pop_first()
+    DCLinkedList.pop_first()
     print(DCLinkedList)
 
+    element = DCLinkedList.get(4)
+    print(element.node)
+    # DCLinkedList.traverse()
+    # DCLinkedList.reverse_traverse()
+
+    tp = DCLinkedList.search(3)
+    print(tp)
+
+    DCLinkedList.set(2, 1000)
+    print(DCLinkedList)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
