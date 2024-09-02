f6ee2a5559f64309d1f4d5ed45ff03e7fdc571bf
Matthew Lozinski <lozinskimatthew0@gmail.com>
2024-09-02 13:10:37 +0200

    Queue in progress v3

--- Queue/Queue.py
+++ Queue/Queue.py
@@ -1,5 +1,46 @@
-from sympy.physics.units import current
-from torch.fft import ifftn
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LL:
+    def __init__(self, value=None):
+        if value = None:
+            self.head = None
+            self.tail = None
+            self.length = 0
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def __str__(self):
+        if self.head == None:
+            return "Empty!"
+        curr = self.head
+        stringed = []
+        stringed.append(str(curr.value))
+        while curr != tail:
+            curr = curr.next
+            stringed.append(str(curr.value))
+        stringed.append(str(curr.value))
+        return ", ".join(stringed)
+
+    def isEmpty(self):
+        if self.head == None:
+            True
+        else:
+            False
+
+
+class QueueList:
+
+    def __init__(self, nd=None):
+        self.ll = LL(nd)
+        if ll.isEmpty():
+
+
 
 
 class Queue:
@@ -16,13 +57,15 @@ class Queue:
         curr = self.items[self.start]
         helper = self.start
         while curr is not None:
-            curr = self.items[self.start]
-            stringed.append(curr)
-            if helper == self.length and self.end != self.length - 1:
+            if helper == self.length - 1 and self.end != self.length - 1:
                 helper = 0
                 stringed.append(str(self.items[0]))
-            curr = self.items[helper + 1]
+            stringed.append(str(curr))
             helper += 1
+            if helper == self.length - 1:
+                stringed.append(str(self.items[self.length - 1]))
+                break
+            curr = self.items[helper]
         return ", ".join(stringed)
 
     def isEmpty(self):
@@ -32,7 +75,9 @@ class Queue:
             return False
 
     def isFull(self):
-        if self.items[self.end + 1] == self.items[self.start]:
+        if self.end + 1 == self.length and self.start == 0:
+            return True
+        elif self.end + 1 == self.start and self.start != 0:
             return True
         else:
             return False
@@ -53,3 +98,13 @@ class Queue:
         else:
             self.items[self.end + 1] = value
             self.end += 1
+
+    def dequeue(self):
+        if self.isEmpty():
+            return "Queue is empty"
+
+        self.items[self.start] = None
+        if self.start == self.length - 1:
+            self.start = 0
+            return
+        self.start += 1
--- Queue/main.py
+++ Queue/main.py
@@ -4,11 +4,15 @@ if __name__ == "__main__":
     q = Queue.Queue(4)
     # print(q.isEmpty())
     q.enqueue(5)
-    q.enqueue(5)
+    q.enqueue(6)
+    q.enqueue(7)
+    q.enqueue(8)
+    print(q.isFull())
     # print(q.start)
     # print(q.end)
     print(q)
-
+    q.dequeue()
+    print(q)
 
 
 
